6321bd2c51a347631beee0eded23b3db3dc6451a
Vlad <vladisla1988@gmail.com>
2021-04-07 12:52:34 +0300

    create start package

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+// import './styles/styles.css';
+import json from './assets/json.json';
+import xml from './assets/data.xml';
+// import Img from './assets/img.png';
+import './styles/scss.scss';
+import './babel';
